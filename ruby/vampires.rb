ae1baaff6d835b9f74a5a64798eebf75c162ac71
bashaliora <elizabethashleykeys@gmail.com>
2016-06-13 23:08:06 -0400

    Fix not equal to operator, fix literal string in to boolean function

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,5 +1,5 @@
 def to_boolean(string)
-	if string == "false" || "False" || "no" || "No"
+	if string == "false" || string == "False" || string == "no" || string == "No"
 		return false
 	else
 		return true
@@ -11,27 +11,27 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "In what year were you born?"
-birth_year = gets.chomp
+birth_year = gets.chomp.to_i
 
 puts "Would you like garlic bread for lunch?"
 likes_garlic = to_boolean(gets.chomp)
 
 puts "Would you like to enroll in our health insurance plan?"
-health_insurance = gets.chomp
+health_insurance = to_boolean(gets.chomp)
 
 
 if age == 2016 - birth_year && (likes_garlic == true || health_insurance == true)
-	return "Probably not a vampire"
-elsif age /= 2016 - birth_year && (likes_garlic == false || health_insurace == false)
-	return "Probably a vampire"
+	puts "Probably not a vampire"
+elsif age != 2016 - birth_year && (likes_garlic == false || health_insurace == false)
+	puts "Probably a vampire"
 elsif age /= 2016 - birth_year && likes_garlic == false && health_insurance == false	
-	return = "Almost certainly a vampire"
+	puts = "Almost certainly a vampire"
 elsif name == "Drake Cula" || "Tu Fang"
-	return = "Definitely a vampire"
+	puts = "Definitely a vampire"
 else
-	return = "Results inconclusive."
-
+	puts = "Results inconclusive."
+end
 
