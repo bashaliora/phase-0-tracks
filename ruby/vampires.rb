9d495a4bf943b88dff1ff27333df61386f3c7fd2
bashaliora <elizabethashleykeys@gmail.com>
2016-06-16 23:55:11 -0400

    Add parntheses to definitely vampire condition - eureka

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,40 +7,39 @@ def to_boolean(string)
 end			
 
 
-puts "What\'s your name?"
-name = gets.chomp
+#while employees_to_log > 0
+	puts 'What\'s your name?'
+	name = gets.chomp
+	
+	puts 'How old are you?'
+	age = gets.chomp.to_i
+	
+	puts 'In what year were you born?'
+	birth_year = gets.chomp.to_i
+	
+	puts 'Would you like garlic bread for lunch?'
+	likes_garlic = to_boolean(gets.chomp.downcase)
+	
+	puts "Would you like to enroll in our health insurance plan?"
+	health_insurance = to_boolean(gets.chomp.downcase)
 
-puts "How old are you?"
-age = gets.chomp.to_i
 
-puts "In what year were you born?"
-birth_year = gets.chomp.to_i
-
-puts "Would you like garlic bread for lunch?"
-likes_garlic = to_boolean(gets.chomp.downcase)
-
-puts "Would you like to enroll in our health insurance plan?"
-health_insurance = to_boolean(gets.chomp.downcase)
+are_they_a_vampire = "Results inconclusive."
 
 
-if name == "Drake Cula" || name == "Tu Fang"
-	puts "Definitely a vampire"
-else	
-	if age == 2016 - birth_year && (likes_garlic == true && health_insurance == true)
-		puts "Probably not a vampire"
-	end	
-	if age != 2016 - birth_year && (likes_garlic == false || health_insurance == false)
-	 	# puts "Probably a vampire"
-		if likes_garlic == false && health_insurance == false
-			if age != 2016 - birth_year && likes_garlic == false && health_insurance == false
-				puts "Almost certainly a vampire"
-			else
-				puts "Results inconclusive."
-			end
-		else
-			puts "Probably a vampire."
-		end		
-	end
+if (age == 2016 - birth_year) && (likes_garlic == true || health_insurance == true)
+	are_they_a_vampire = "Probably not a vampire"
 end
+if (age != 2016 - birth_year) && (likes_garlic == false || health_insurance == false)
+	are_they_a_vampire = "Probably a vampire"
+end
+if (age != 2016 - birth_year) && likes_garlic == false && health_insurance == false
+		are_they_a_vampire = "Almost certainly a vampire"
+end		
+if (name == "Drake Cula" || name == "Tu Fang")
+		are_they_a_vampire = "Definitely a vampire"
+end
+	
 
+puts are_they_a_vampire
 
