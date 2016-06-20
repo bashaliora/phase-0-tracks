1245243fbb0c97764d754af37e63876b0b4dcba9
bashaliora <elizabethashleykeys@gmail.com>
2016-06-19 23:49:56 -0400

    Replace method used on line 28 to slice

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -5,37 +5,37 @@
 # committed the crime, add additional calls to prove it.
 
  "iNvEsTiGaTiOn".swapcase
- => “InVeStIgAtIoN”
+ #=> “InVeStIgAtIoN”
 
  "zom".insert(2, "o")
- => “zoom”
+ # => “zoom”
 
- "enhance".center
- => "    enhance    "
+ "enhance".center(20)
+ # => "    enhance    "
 
- "Stop! You’re under arrest!".upcase
- => "STOP! YOU’RE UNDER ARREST!"
+ "Stop! You\’re under arrest!".upcase
+ # => "STOP! YOU’RE UNDER ARREST!"
 
  "the usual".concat(" suspects")
- => "the usual suspects"
+ # => "the usual suspects"
 
  " suspects".insert(0, 'the usual')
- => "the usual suspects"
+ # => "the usual suspects"
 
  "The case of the disappearing last letter".chop
- => "The case of the disappearing last lette"
+ # => "The case of the disappearing last lette"
 
  "The mystery of the missing first letter".slice(1..38)
- => "he mystery of the missing first letter"
+ # => "he mystery of the missing first letter"
 
  "Elementary,    my   dear        Watson!".strip
- => "Elementary, my dear Watson!"
+ # => "Elementary, my dear Watson!"
 
  "z".ord
- => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+ # => 122 
+ # (What is the significance of the number 122 in relation to the character z?)
 
  "How many times does the letter 'a' appear in this string?".count "a"
- => 4
+ # => 4
 
- 
+end
