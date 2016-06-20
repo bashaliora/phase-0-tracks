39448ad0625b9c747c98c0ef249481080291ddb8
bashaliora <elizabethashleykeys@gmail.com>
2016-06-20 01:03:28 -0400

    Write pseudocode for interior design program

---
+++ ruby/hashes.rb
@@ -0,0 +1,11 @@
+# should first ask an interior designer for client's name
+#  ask for age, number of children, decor theme, favorite color,
+#  wood or carpet flooring
+# convert designer input to appropriate data type
+# print all answers back to the screen when form is complete
+# allow designer one chance to update a key
+#  if designer says 'none' skip it, do not loop
+#  if designer enters "decor_theme" (ex), program should ask for new value and update :decor_theme key
+# print latest version of hash, exit program
+
+
