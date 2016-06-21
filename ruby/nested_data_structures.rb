6f94aa9047b4e688fe6420c8c37c41536569f332
bashaliora <elizabethashleykeys@gmail.com>
2016-06-21 15:00:27 -0400

    Remove comment for pick up

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,77 +2,81 @@
 #use real life item
 #print a few deeply nested pieces of data
 
+
+
 #Nested Data Structure For My Comic Book Collection
 
 #Comic Book Room
 #Alphabetized Comic Boxes
 #Comic Book Title Dividers
-#Volume Dividers
+#Volumes
 
 comic_book_room = { #hash named comic_book_room
 	box_a: { #hash for box a
 			aloha_hawaiian: {
-			actual_title: 'Aloha, Hawaiian Dick'
-			 issues_owned: {
-				total_issues: 3
-			},
-
+			actual_title: 'Aloha, Hawaiian Dick',
+			issues_owned: {
+				total_issues: 3,
+				},
+			
 			volumes: [
 				'1'
-				]
-		},	
+				],
+			},	
+	
 
 			animal_man: { #hash for animal man
-			actual_title: 'Animal Man'
+			actual_title: 'Animal Man',
 			issues_owned: {
-				total_issues: 80
+				total_issues: 80,
 			},
 
 			volumes: [ #array for volumes available
 				'1','2'
-				]
-		},
+				],
+			},
 
 			ant_man: {
-			actual_title: 'Ant Man'
+			actual_title: 'Ant Man',
 			issues_owned: {
-				total_issues: 48
+				total_issues: 48,
 			},
 			
 			volumes: [
 				'1'
-				]	
-		},
+				],
+			},
+	},	
 	
 	
 	box_b: {
 			batman: {
-			actual_title: 'Batman'
+			actual_title: 'Batman',
 			issues_owned: {
-				total_issues: 211
+				total_issues: 211,
 			},
 
 			volumes: [
 				'1','2','3'
-				]
-		},
+				],
+			},
 
 
 			bitch_planet: {
-			actual_title: 'Bitch Planet'
+			actual_title: 'Bitch Planet',
 			issues_owned: {
-				total_issues: 8
+				total_issues: 8,
 			},
 			
 			volumes: [
 				'1'
-				]		
-		},
+				],		
+			},
 
 			beauty: {
-			actual_title: 'Beauty'
+			actual_title: 'Beauty',
 			issues_owned: {
-				total_issues: 3
+				total_issues: 3,
 			},
 			
 			volumes: [
@@ -81,8 +85,11 @@ comic_book_room = { #hash named comic_book_room
 			}
 	}			
  }
+ 
 
- p comic_book_room
-
-
-#pick up tomorrow with why you're getting an unexpected identifier error message
+comic_book_room[:box_a][:animal_man][:actual_title]
+comic_book_room[:box_b][:batman][:actual_title].reverse
+comic_book_room[:box_b][:bitch_planet][:issues_owned][:total_issues]
+comic_book_room[:box_b][:batman][:volumes][0]
+comic_book_room[:box_b][:batman][:volumes]
+ 
