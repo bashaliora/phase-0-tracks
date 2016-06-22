2456bcb4b60a11422ad7a4f1fe5051c799f49494
bashaliora <elizabethashleykeys@gmail.com>
2016-06-22 00:39:52 -0400

    Define method to move vowel to next vowel

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,9 +8,70 @@
 # How will you deal with the fact that some letters are uppercase?
 # How will you handle edge cases?
 
+#liz keys should become moa lizt
+
+
 puts "Hello Agent, please enter your true name."
 agent_name = gets.chomp
 
-reverse_name = agent_name.split(' ').reverse
-p reverse_name
+
+#take agent name and separate into characters
+#if vowel, remove from array and move to new array
+#move vowel array to new letter
+#if consonant, move to new letter
+#merge arrays
+#join arrays, convert to string
+#reverse name
+
+separated_name = agent_name.split('')
+p separated_name
+
+vowels = [
+	'a',
+	'e',
+	'i',
+	'o',
+	'u'
+]
+
+
+def next_vowel(vowels)
+	if vowels == "a"
+		return "e"
+	elsif vowels == "e"
+		return "i"
+	elsif vowels == "i"
+		return "o"
+	elsif vowels == "o"
+		return "u"
+	elsif vowels == "u"
+		return "a"
+	end	
+end	
+
+p next_vowel('a')
+p next_vowel('e')
+p next_vowel('i')
+p next_vowel('o')
+p next_vowel('u')
+
+
+
+consonants = separated_name - vowels
+p consonants
+
+
+consonants.map! do |letter|
+	letter.next
+end
+
+
+
+
+
+
+p consonants
+
+
+
 
