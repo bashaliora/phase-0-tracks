b0a8bc7ad06bc32829aaa60422a5fc9c168e120d
bashaliora <elizabethashleykeys@gmail.com>
2016-06-21 21:55:48 -0400

    Add pseudocode and array with each call

---
+++ ruby/iteration.rb
@@ -0,0 +1,34 @@
+#write a meaningful array and then iterate using .each and .map
+#write a meaningful hash and iterate using .each
+
+animals_in_zoo = [
+	'lion',
+	'tiger',
+	'bear',
+	'zebra',
+	'red panda'
+]
+
+puts "Original data:"
+p animals_in_zoo
+
+index = 0
+animals_in_zoo.each do |cage_number|
+	puts cage_number
+end	
+
+
+puts "After .each call:"
+p cage_number
+
+
+
+# favorite_animals = {
+# 	:liz => 'owl', 
+# 	:michael => 'gorilla', 
+# 	:aidan => 'penguin', 
+# 	:landon => 'lemur'
+# }
+
+
+
