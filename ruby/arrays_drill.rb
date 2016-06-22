15e517d076e5a518a383ae80ab1c630e985b8623
bashaliora <elizabethashleykeys@gmail.com>
2016-06-22 19:30:04 -0400

    Define two methods for building arrays

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,17 @@
+
+
+
+
+
+
+def make_array(value_1, value_2, value_3)
+	make_array = [value_1, value_2, value_3]
+end	
+
+def add_array(array, value)
+	array << value
+end
+
 colors = []
 p colors
 
@@ -24,4 +38,18 @@ find_orange = colors.include?('orange')
  else 
  	puts "orange is not in this array!"
  end	
+
+numbers = [1,7,8]
+p numbers
+
+
+
+colors_and_numbers = colors.push(numbers).flatten!
+p colors_and_numbers
+
+
+p make_array(1, true, 'blue')
+p add_array([45, 46], 47)
+p add_array(['grass', 'trees'], 'sky')
+
 
