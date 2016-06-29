a33f9e2807651509270359c3b59eea9a91e95aff
Elizabeth Keys <elizabethashleykeys@gmail.com>
2016-06-29 00:04:53 -0400

    Add progress on calling methods in puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,8 +1,28 @@
 class Puppy
+	
+  def initialize()
+  	p "Initializing a new puppy instance..."
+  end			
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
+  end
+
+  def speak(number_of_barks)
+  	number_of_barks.times { puts "woof!" }
+  end
+  
+  def roll_over
+  	puts "roll over!"
+  end
+  
+  def dog_years(human_age)
+  	dog_age = human_age * 7
+  end	
+
+  def high_jump(height_of_jump)
+  	puts "i jumped #{height_of_jump} feet!"
   end
 
 end
@@ -11,3 +31,9 @@ end
 
 # DRIVER CODE
 
+spot = Puppy.new
+spot.fetch("ball")
+spot.speak(4)
+spot.roll_over
+spot.dog_years(17)
+spot.high_jump(3)
