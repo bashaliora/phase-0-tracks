c25a8eb8bbc79b049fa3f8ee38159591ce9ca3eb
Elizabeth Keys <elizabethashleykeys@gmail.com>
2016-07-01 15:44:18 -0400

    Add progress for class and instance methods to test with rspec

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,24 @@
+# CHALLENGE CODE
+
+class TodoList
+
+  def initialize(list)
+    @list = list
+  end
+
+  def get_items
+    @list
+  end
+
+  def add_item(item)
+    @list << item
+  end
+
+  def delete_item(item)
+    @list.delete(item)
+  end
+
+  def get_item(i)
+    @list[i]
+  end
+end
