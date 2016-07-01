f9f384559224eebc16c2fe4c5f7489eef055bdda
Elizabeth Keys <elizabethashleykeys@gmail.com>
2016-06-30 21:12:32 -0400

    Create Wrestler class and initialize, powerbomb, and finisher methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -10,5 +10,29 @@
 # Name is a string, height is a string, weight is an integer
 # Define a powerbomb method, which will print a string
 # Definte a finisher method, will use a string for the finisher name
-# Add getter method, using reader to allow access of name, age, height, and weigtht
+# Add getter method, using reader to allow access of name, height, and weigtht
 # Add an accessor method that will allow the finisher to be changed outside of the class
+
+
+class Wrestler
+
+  attr_reader :name, :height, :weight
+  attr_accessor :finisher
+
+  def initialize(name, height, weight)
+  	@name = name
+  	@height = height
+  	@weight = weight
+  	puts "Initializing a new Wrestler!"
+  end
+  
+  def powerbomb
+  	puts "#{name} with a BRUTAL powerbomb!"
+  end
+  
+  def finisher(move_name)
+  	@finisher = finisher
+  	puts "{name} with a three count after the devestating #{finisher}"
+  end	
+
+end
