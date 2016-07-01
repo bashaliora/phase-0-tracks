393a37e6c947d08e611f39531c6de7cec6764b88
Elizabeth Keys <elizabethashleykeys@gmail.com>
2016-06-30 21:05:58 -0400

    Write pseudocode for Wrestler class

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+#WRESTLER CLASS
+
+#PSEUDOCODE
+
+# Create the class Wreslter
+# Input the name of the class
+
+# Create three methods that will give attributes to the class
+# Define the initialize method with name, height, and weight
+# Name is a string, height is a string, weight is an integer
+# Define a powerbomb method, which will print a string
+# Definte a finisher method, will use a string for the finisher name
+# Add getter method, using reader to allow access of name, age, height, and weigtht
+# Add an accessor method that will allow the finisher to be changed outside of the class
